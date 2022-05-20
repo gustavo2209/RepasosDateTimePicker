dc303a7ae5653e0caa2895f5429ab5745a30b5d0
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-19 19:27:17 -0500

    actualizaciones

--- RepasosDateTimePicker/Form1.Designer.cs
+++ RepasosDateTimePicker/Form1.Designer.cs
@@ -32,6 +32,9 @@ namespace RepasosDateTimePicker
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -50,6 +53,7 @@ namespace RepasosDateTimePicker
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -58,11 +62,37 @@ namespace RepasosDateTimePicker
             this.textBox1.Size = new System.Drawing.Size(261, 20);
             this.textBox1.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(301, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 250);
+            this.label1.TabIndex = 3;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(334, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 196);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 65);
+            this.textBox2.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 134);
+            this.ClientSize = new System.Drawing.Size(606, 273);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
@@ -82,6 +112,9 @@ namespace RepasosDateTimePicker
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RepasosDateTimePicker/Form1.cs
+++ RepasosDateTimePicker/Form1.cs
@@ -20,6 +20,22 @@ namespace RepasosDateTimePicker
         private void Form1_Load(object sender, EventArgs e)
         {
             dateTimePicker1.CustomFormat = "dd/MM/yyyy - dddd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = dateTimePicker1.Text;
+            //textBox1.Text = dateTimePicker1.Value.ToString();
+
+            DateTime dt = dateTimePicker1.Value;
+            string s = dt.Day + "/" + dt.Month + "/" + dt.Year;
+            textBox1.Text = s;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox2.Text = "Inicio: " + e.Start.ToShortDateString() + "\r\nFin: " + e.End.ToShortDateString();
         }
     }
 }
